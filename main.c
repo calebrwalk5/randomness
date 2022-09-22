bc99584e7859a6daf72383cfecb2fd2c1a0d427f
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-22 12:24:02 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int main(){
   srand(time(NULL));
   int o_1875464fddc993b2fb3b2f28303f8b6a=rand();
   int o_bfa01ebb18f02a1894d054cd85fdc228=o_1875464fddc993b2fb3b2f28303f8b6a * rand() - rand();
-  printf("\x25""d\012",o_bfa01ebb18f02a1894d054cd85fdc228);
+  for (int i = 0; i < rand(); i++) {
+    printf("\x25""d\012",o_bfa01ebb18f02a1894d054cd85fdc228);
+  }
   return (0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00);
 }
